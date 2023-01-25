621d4a215ec7be3b520882db57217a7aa47e3402
Kevin Maykel <kevinmaykelgyn@gmail.com>
2023-01-25 18:55:31 -0300

    dev do algoritimo
    comecei a desenvolver o algorito do cinema

---
+++ Algoritimo Cinema/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int id, ingressos;
+
+    printf("Digite o id: \n");
+    scanf("%d", &id);
+
+    printf("Digite a quantidade de ingrssos: \n");
+    scanf("%d", &ingressos);
+    
+
+
+    return 0;
+}
