9566146a6bfe28c78a648f2ca13269e4d37b252b
Kevin Maykel <kevinmaykelgyn@gmail.com>
2023-01-30 10:09:01 -0300

    alteração das funções

--- Algoritimo Cinema/main.c
+++ Algoritimo Cinema/main.c
@@ -2,11 +2,83 @@
 #include <string.h>
 #include <conio.h>
 
-int main(void)
+/*
+int assentos(char sala[15][20])
 {
-    char dia_da_semana[56];
-    int id, quantidade_ingressos, reservas = 0, linha = 0, coluna = 0, sala[25][30];
+    for (int linha = 0; linha < 15; linha++)
+        {
+            for (int coluna = 0; coluna < 20; coluna++)
+            {
+                sala[linha][coluna] = 0;
+                printf("%d ", sala[linha][coluna]);
+            }
+            printf("\n");
+        }
+    return 0;
+}
+*/
 
+void reservar_assento(int linhas, int colunas, int matriz[][colunas]) {
+  
+  int linha, coluna;
+  
+  printf("Digite a linha e coluna do assento a ser reservado: ");
+  scanf("%d%d", &linha, &coluna);
+
+  if (matriz[linha][coluna] == 0) 
+  {
+    matriz[linha][coluna] = 1;
+    printf("Assento (%d,%d) reservado com sucesso.\n", linha, coluna);
+  } else 
+  {
+    printf("Assento (%d,%d) já está ocupado.\n", linha, coluna);
+  }
+}
+
+void exibir_assentos_disponiveis(int linhas, int coluna, char sala[15][20]) {
+  int contador = 0;
+  printf("Assentos disponíveis:\n");
+
+  for (int linha = 0; linha < 10; linha++)
+        {
+            for (int coluna = 0; coluna < 10; coluna++)
+            {
+                sala[linha][coluna] = 0;
+                printf("%d ", sala[linha][coluna]);
+            }
+            printf("\n");
+        }
+/*
+  for (int i = 0; i < linhas; i++) {
+    for (int j = 0; j < 20; j++) {
+      if (matriz[i][j] == 0) {
+        printf("(%d,%d) ", i, j);
+        printf("\n");
+        contador++;
+      }
+    }
+  }
+  if (contador == 0) {
+    printf("Nenhum assento disponível.\n");
+  }
+  */
+}
+
+int assento_ocupado(int linhas, int colunas, int linha, int coluna, int matriz[][colunas]) {
+  if (matriz[linha][coluna] == 1) {
+    return 1;
+  }
+  return 0;
+}
+
+
+
+
+
+int main()
+{
+    char dia_da_semana[56], sala[15][20];
+    int id, quantidade_ingressos, reservas = 0, linha, colunas, matriz[15][20];
     /*
     printf("Digite o id: \n");
     scanf("%d", &id);
@@ -20,54 +92,8 @@ int main(void)
     }
     printf("Digite o dia da semana: \n");
     scanf("%s",&dia_da_semana);
-
     */
-    for (int l = 0; l < 25; l++)
-    {
-        for (int c = 0; c < 30; c++)
-        {
-            sala[l][c] = 0;
-        }
-        
-    }
+    /*Chama a função assentos e Printa a matriz com os valores*/
     
-    do{
-        printf("0  1  2  3  4  5  6  7  8  9  10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25  26  27  28  29\n");
-        
-        for (int l = 0; l < 25; l++)
-        {
-            
-            for (int c = 0; c < 30; c++)
-            {
-                printf("%d\n", sala[l][c]);
-            }
-            printf("\n%d\n", l);
-            printf("Digite a linha e coluna da poltrona desejada: ");
-            scanf("%d %d", & linha, &coluna);
-
-            if (linha < 0 || linha > 24)
-            {
-                printf("\nLinha invalidada. Digite outra linha valida\n");
-            }
-            else if (coluna < 0 || coluna > 29)
-            {
-                printf("\nColuna invalidada. Digite outra coluna valida\n");
-            }
-            else if (sala[linha][coluna] == 1)
-            {
-                printf("\nPoltrona invalida. Escolha outra\n");
-            }
-            else
-            {
-                sala[linha][coluna] = 1;
-                printf("\nReserva realizada com sucesso\n");
-                reservas++;
-            }
-            
-        }
-        
-    }while (reservas < 750 && linha >= 0 || coluna >= 0);
-  
-
     return 0;
 }
