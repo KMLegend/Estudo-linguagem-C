43e2030b89bc2e408e2e67603cf9fbc449dbc256
Kevin Maykel <kevinmaykelgyn@gmail.com>
2023-01-30 11:22:45 -0300

    modo yasmin modificado

--- Algoritimo Cinema/main.c
+++ Algoritimo Cinema/main.c
@@ -1,99 +1,156 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <conio.h>
+#include <math.h>
 
-/*
-int assentos(char sala[15][20])
+
+
+int main (void)
 {
-    for (int linha = 0; linha < 15; linha++)
-        {
-            for (int coluna = 0; coluna < 20; coluna++)
-            {
-                sala[linha][coluna] = 0;
-                printf("%d ", sala[linha][coluna]);
-            }
-            printf("\n");
-        }
-    return 0;
-}
-*/
+	int Caso_cadeira, i, j, lin, col, condi, n, t, x, p;
+	int poltrona[10] [15];
+	char ascii = 79;
+	char asciii = 158;
+	
+	for(i=0 ; i<10; i++)
+	{
+		for(j=0 ; j<15 ; j++)
+		{
+			poltrona [i] [j]=0;
+		}
+	}
 
-void reservar_assento(int linhas, int colunas, int matriz[][colunas]) {
-  
-  int linha, coluna;
-  
-  printf("Digite a linha e coluna do assento a ser reservado: ");
-  scanf("%d%d", &linha, &coluna);
-
-  if (matriz[linha][coluna] == 0) 
-  {
-    matriz[linha][coluna] = 1;
-    printf("Assento (%d,%d) reservado com sucesso.\n", linha, coluna);
-  } else 
-  {
-    printf("Assento (%d,%d) já está ocupado.\n", linha, coluna);
-  }
-}
-
-void exibir_assentos_disponiveis(int linhas, int coluna, char sala[15][20]) {
-  int contador = 0;
-  printf("Assentos disponíveis:\n");
-
-  for (int linha = 0; linha < 10; linha++)
-        {
-            for (int coluna = 0; coluna < 10; coluna++)
-            {
-                sala[linha][coluna] = 0;
-                printf("%d ", sala[linha][coluna]);
-            }
-            printf("\n");
-        }
-/*
-  for (int i = 0; i < linhas; i++) {
-    for (int j = 0; j < 20; j++) {
-      if (matriz[i][j] == 0) {
-        printf("(%d,%d) ", i, j);
-        printf("\n");
-        contador++;
-      }
-    }
-  }
-  if (contador == 0) {
-    printf("Nenhum assento disponível.\n");
-  }
-  */
-}
-
-int assento_ocupado(int linhas, int colunas, int linha, int coluna, int matriz[][colunas]) {
-  if (matriz[linha][coluna] == 1) {
-    return 1;
-  }
-  return 0;
-}
-
-
-
-
-
-int main()
-{
-    char dia_da_semana[56], sala[15][20];
-    int id, quantidade_ingressos, reservas = 0, linha, colunas, matriz[15][20];
-    /*
-    printf("Digite o id: \n");
-    scanf("%d", &id);
-
-    printf("Digite a quantidade de ingrssos: \n");
-    scanf("%d", &quantidade_ingressos);
-    if (quantidade_ingressos > 3 || quantidade_ingressos < 1)
-    {
-        printf("A quantidade maxima de ingressos por id e´ de 3 ingressos: \n");
-        scanf("%d", &quantidade_ingressos);
-    }
-    printf("Digite o dia da semana: \n");
-    scanf("%s",&dia_da_semana);
-    */
-    /*Chama a função assentos e Printa a matriz com os valores*/
+    n=0;
+    printf("\tC\n\tO\n\tL\n\tU\n\tN\n\tA\n");
     
-    return 0;
+    printf("LINHA");
+    printf("   0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 \n");
+            for(i=0 ; i<10; i++)
+    {
+                    printf("\t");
+                    n==1;
+                    printf("%d ",n);
+                    n++;
+                
+                for(j=0 ; j<15 ; j++)
+                {
+                    if(poltrona[i] [j]==0)
+                    {
+                        printf("%c ",ascii);
+                    }	
+                    else
+                        printf("%c ",asciii);
+                }
+                
+            printf("\n");
+    }
+    printf("\n");
+    printf("-------------------------------------------------------------\n");
+
+
+	do
+	{			
+			printf("[1] - Escolher Cadeira\n");
+			printf("[2] - Ver disposicao das cadeiras\n");
+			printf("[3] - Cancelar cadeira\n\n");
+			printf("[4] - Escolha concluida\n\n");
+			scanf("%d", &Caso_cadeira);
+			
+			switch(Caso_cadeira)
+			{	
+				case 1: //Escolher Cadeira
+
+					printf("Fila: ");
+                    scanf("%d", &lin);
+                
+                    printf("Coluna: ");
+                    scanf("%d",&col);
+                    
+                    if (lin<10 && col<15)
+                    {
+                        if (poltrona [lin] [col]==0)
+                        {
+                            poltrona [lin] [col]=1;
+                        }
+                        else
+                        {
+                            printf("***ATENCAO CADEIRA OCUPADA***\n\n");
+                        }
+                    }
+                    else
+                    {
+                        printf("***CADEIRA INEXISTENTE***");
+                    }
+
+
+                    printf("\n");
+                    printf("-------------------------------------------------------------\n");
+                    
+                    break;
+				case 2: //ver caideiras
+					n=0;
+					printf("\tC\n\tO\n\tL\n\tU\n\tN\n\tA\n");
+					
+					printf("LINHA");
+					printf("   0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 \n");
+							for(i=0 ; i<10; i++)
+					{
+									printf("\t");
+									n==1;
+									printf("%d ",n);
+									n++;
+								
+								for(j=0 ; j<15 ; j++)
+								{
+									if(poltrona[i] [j]==0)
+									{
+										printf("%c ",ascii);
+									}	
+									else
+										printf("%c ",asciii);
+								}
+								
+							printf("\n");
+					}
+					printf("\n");
+					printf("-------------------------------------------------------------\n");
+					break;
+				case 3: //esvaziar cadeiras
+					printf("O que deseja fazer?\n");
+					printf("[1] - Cancelar uma cadeira?\n");
+					printf("[2] - Cancelar todas as cadeiras?\n");
+					
+					scanf("%d",&condi);
+						if(condi == 1)
+						{
+							printf("\nFila: ");
+							scanf("%d", &lin);
+							printf("\nColuna: ");
+							scanf("%d",&col);
+							if(lin<10 && col<10)
+							{
+								poltrona [lin] [col]=0;
+							}
+							else
+							{
+								printf("***CADEIRA INEXISTENTE***\n");
+							}
+						}
+						else 
+						for(i=0 ; i<10; i++)
+						{
+							for(j=0 ; j<15 ; j++)
+							{
+								poltrona [i] [j]=0;
+							}
+						}
+					printf("-------------------------------------------------------------\n");
+					break;
+				default:
+					Caso_cadeira=0;
+					
+			}
+		
+	}while(Caso_cadeira!=0);
+	system("pause");
+	return(0);
 }
